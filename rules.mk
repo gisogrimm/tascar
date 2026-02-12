b42d5a8b171daffde5194145f25d852d721ebf99
Giso Grimm (thetys) <g.grimm@uni-oldenburg.de>
2026-02-12 13:21:16 +0100

    use direct linking for lsl

--- rules.mk
+++ rules.mk
@@ -39,7 +39,9 @@ ifeq ($(UNAME_S),Darwin)
   CXXFLAGS += -I/opt/homebrew/include -DISMACOS
   CPPFLAGS += -I/opt/homebrew/include
   LDFLAGS += -L/opt/homebrew/lib
-  CXXFLAGS += -framework LSL
+  CXXFLAGS += $(addprefix -I,$(dir $(shell find /opt/homebrew/ -name lsl_cpp.h)))
+  LDFLAGS += $(addprefix -L,$(dir $(shell find /opt/homebrew/ -name liblsl*.dylib)))
+#-framework Lsl
 endif
 endif
 
