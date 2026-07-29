83145604c3e9eb569ddbfffdc64053d354f09935
tobias <herzke@hz-ol.de>
2026-07-29 10:29:06 +0200

    Fix GITMODIFIED on Windows Github runners
    This git repository contains two symbolic links. When checking for git
    modifications on Github Windows runners with msys2 git, then these two
    symbolic links always show up as modified. Ignore modifications detected
    in these two links to avoid appending "-modified" to the version number.

--- get_version.sh
+++ get_version.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 VERSION=$(cat config.mk | sed -e '2 ! d' -e 's/.*=//1')
-
-GITMODIFIED=$(test -z "`git status --porcelain -uno`" || echo "-modified")
+# If the source tree contains modifications, append "-modified" to the version string.
+# But ignore alleged symlink modifications detected on Github Windows runners.
+GITMODIFIED=$(test -z "`git status --porcelain -uno | grep -v T.external_libs/libmysofa-1.3.2/share/default.sofa | grep -v D.libtascar/tascar`" || echo "-modified")
 COMMITHASH=$(git log -1 --abbrev=7 --pretty='format:%h')
 LATEST_RELEASETAG=$(git tag -l "release*" |tail -1)
 COMMIT_SINCE_RELEASE=$(git rev-list --count ${LATEST_RELEASETAG}..)
